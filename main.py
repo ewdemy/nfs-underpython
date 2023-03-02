a8cc847c75bb8bb4834796c1fe183a45cf4927ca
ewdemy <ewdemy@hotmail.com>
2023-03-02 07:42:11 -0300

    Adicionado icone e e música de fundo

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
-pygame.init()
+from pygame import mixer
 
-janela = pygame.display.set_mode((800,600))
+pygame.init()
+mixer.init()
+
+janela = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Need For Speed UnderPython")
 x = 350
 y = 200
@@ -9,34 +12,41 @@ y = 200
 distancia = 10
 janela_aberta = True
 
+mixer.music.load("./sound/trilha.mp3")
+mixer.music.set_volume(0.5)
+mixer.music.play()
+
 fundo = pygame.image.load("./img/fundo.png")
+icon = pygame.image.load("./img/icon.png")
 carro_vermelho = pygame.image.load("./img/carro_vermelho.png")
 carro_amarelo = pygame.image.load("./img/carro_amarelo.png")
+carro_cinza = pygame.image.load("./img/carro_cinza.png")
 
-fundo = pygame.transform.scale(fundo, (800,600))
-carro_vermelho = pygame.transform.scale(carro_vermelho, (100,180))
+fundo = pygame.transform.scale(fundo, (800, 600))
+carro_vermelho = pygame.transform.scale(carro_vermelho, (100, 180))
+carro_cinza = pygame.transform.scale(carro_cinza, (100, 180))
+
+pygame.display.set_icon(icon)
 
 while(janela_aberta):
     pygame.time.delay(50)
     for event in pygame.event.get():
-        if(event.type == pygame.QUIT):
+        if event.type == pygame.QUIT:
             janela_aberta = False
 
         comandos = pygame.key.get_pressed()
-    if(comandos[pygame.K_UP]):
-        y-= distancia
-    if (comandos[pygame.K_DOWN]):
+    if comandos[pygame.K_UP]:
+        y -= distancia
+    if comandos[pygame.K_DOWN]:
         y += distancia
-    if (comandos[pygame.K_RIGHT]):
+    if comandos[pygame.K_RIGHT]:
         x += distancia
-    if (comandos[pygame.K_LEFT]):
+    if comandos[pygame.K_LEFT]:
         x -= distancia
 
-    janela.blit(fundo,(0,0))
-    janela.blit(carro_vermelho,(x,y))
-
+    janela.blit(fundo, (0, 0))
+    janela.blit(carro_vermelho, (x, y))
 
     pygame.display.update()
-
 
 pygame.quit()
